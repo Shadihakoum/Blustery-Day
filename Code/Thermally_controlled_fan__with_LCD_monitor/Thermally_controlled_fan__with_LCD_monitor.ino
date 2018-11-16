37ebbf560331e873e6fccd19311ee135afc471b1
Ashten <ashten.bontrager@pcc.edu>
2018-11-15 18:54:29 -0800

    Updated lines controlling button behavior, added delay for stability(when handling button press), added then removed lines for testing button behavior(can re-add later)

--- Code/Thermally_controlled_fan__with_LCD_monitor/Thermally_controlled_fan__with_LCD_monitor.ino
+++ Code/Thermally_controlled_fan__with_LCD_monitor/Thermally_controlled_fan__with_LCD_monitor.ino
@@ -4,6 +4,8 @@
 #include <Wire.h>
 #include <SPI.h>
 
+const int led = 13; // Pin 32 on chip.
+
 // Sets the pin number for the sensor at analog pin 41.
   const int Temp_Sensor = A5; // Pin 41 on chip
 
@@ -30,8 +32,11 @@
   int Up_Btn_LastState = 0;
   int Last_Unit_Btn_State = 0;
 
+// Initializes the variable temperature, to store the analog temperature from the sensor
+  int Temperature;
+
 // Sets the intial threshold value to 23 degrees C
-  int Set_Temp = 23;
+  int Set_Temp = 35;
 
 // Sets the initial temperature units to C
   char units = 'C';
@@ -45,16 +50,17 @@
 
   void setup() {
 // Setup serial port to opperate at 9600 bits per second
-  Serial.begin(9600);
+  Serial.begin(19200);
 
-// Initialize input pins 41, 8,1 
+// Initialize input pins 
   pinMode(Temp_Sensor, INPUT) ; 
   pinMode(Dwn_Btn_Pin, INPUT);
   pinMode(Up_Btn_Pin, INPUT);
   pinMode(Unit_Btn_Pin, INPUT);
 
-// Initialize fan control pin
+// Initialize fan control pin and test led
   pinMode(Fan_Control, OUTPUT);
+  pinMode(led, OUTPUT);
 //pinMode(LCD_0, OUTPUT);
 //pinMode(LCD_1, OUTPUT);
 //pinMode(LCD_2, OUTPUT);
@@ -68,16 +74,17 @@
   lcd.begin(16, 2);
 
 // Prints the, constant, header of the LCD
-  lcd.print("Temp. Set Temp.");
+  lcd.print("Current Desired");
 
 }
 
   void loop() {
 // read the value from the sensor at pin 41 and store as a 16bit integer 
-  uint16_t  Temp = analogRead(Temp_Sensor);
-
+  Temperature = analogRead(Temp_Sensor);
+  Temperature = Temperature/2 - 13;
+  
 // Mapping output voltage from temperature sensor to corresponding temperature in degrees C 
-  int Convert_Temp = map(Temp_Sensor, -490, 1500, -55, 150);
+  //int Convert_Temp = map(Temperature, -490, 1500, -55, 150);
   
 // read the input pin for user buttons
   Dwn_Btn_State = digitalRead(Dwn_Btn_Pin);
@@ -85,33 +92,32 @@
   Unit_Btn_State = digitalRead(Unit_Btn_Pin);
 
 // check if user has pressed the down button
-  if (Dwn_Btn_State != Dwn_Btn_LastState){
+  if (Dwn_Btn_State == HIGH){
   // Decrement set temp.
-    Set_Temp--;
+    Set_Temp = Set_Temp - 1;
   }
-// Update state of Dwn Btn
-  Dwn_Btn_LastState = Dwn_Btn_State;
 
 // Check if user has pressed the up button
-  if (Up_Btn_State != Up_Btn_LastState){
+  if (Up_Btn_State == HIGH){
   // Increment set temp
-    Set_Temp++;
+    Set_Temp = Set_Temp + 1;
   }
-// Udate state of Up Btn
-  Up_Btn_LastState = Up_Btn_State;
-
-// Check if the value is high or low
-  if (Unit_Btn_State == LOW){
+  
+// Check if the user wantes degrees F or C
+  if (Unit_Btn_State == HIGH){
     if(Unit_Btn_State != Last_Unit_Btn_State){
   // Sets the temperature unit as  degrees F
     units = 'F';
 
+    digitalWrite(led, HIGH);
+
   // Convert threshold and sensor temperature to degrees F
     Set_Temp_F = (Set_Temp * (9/5)) + 32;
-    Convert_Temp_F = (Convert_Temp * (9/5)) + 32;
+  //  Convert_Temp_F = (Convert_Temp * (9/5)) + 32;
+  Convert_Temp_F = (Temperature * (9/5)) + 32;
 
   // Check the temperature and turn the fan off or on
-     if (Convert_Temp_F >= 73){
+     if (Convert_Temp_F >= Set_Temp_F){
       digitalWrite(Fan_Control, HIGH); 
      }
      else{
@@ -122,20 +128,22 @@
     lcd.setCursor(0, 1);
 
   // Create message to be printed to LCD
-    sprintf(LCD_Output, "%3d %3d char(223)%c", Convert_Temp_F, Set_Temp_F, units);
+    sprintf(LCD_Output, "%3d %c    %3d %c", Convert_Temp_F, units, Set_Temp_F, units);
 
   // Print Temperatures to LCD
     lcd.print(LCD_Output);
 
   // Sets current and last state equal for futur use 
      Last_Unit_Btn_State = Unit_Btn_State;
-  }
-  else{
+
+    }
+    else{
   // sets the temperature unit as degrees C
     units = 'C';
-
+    
   // Check the temperature and turn the fan off or on
-     if (Convert_Temp >= 23){
+  //  if (Convert_Temp >= Set_Temp){
+      if (Temperature >= Set_Temp){
       digitalWrite(Fan_Control, HIGH); 
      }
      else{
@@ -146,10 +154,15 @@
     lcd.setCursor(0, 1);
 
   // Create message to be printed to LCD
-    sprintf(LCD_Output, "%3d %3d char(223)%c", Convert_Temp, Set_Temp, units);
+  // sprintf(LCD_Output, "%3d %c    %3d %c", Convert_Temp,units, Set_Temp, units);
+  sprintf(LCD_Output, "%3d %c    %3d %c", Temperature, units, Set_Temp, units);
 
   // Print Temperatures to LCD
-    lcd.print(LCD_Output);  
-  }
+    lcd.print(LCD_Output);
+
+  // Sets current and last state equal for futur use 
+    Last_Unit_Btn_State = Unit_Btn_State + 1;
+    }
+    delay(2000);
   }
 }
